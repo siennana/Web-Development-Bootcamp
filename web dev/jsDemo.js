c5e509dcf7499c0614d7c987675d9a8e8b309800
Sienna Brown <31221385+siennana@users.noreply.github.com>
2020-08-25 16:54:43 -0700

    JS practice

--- web dev/jsDemo.js
+++ web dev/jsDemo.js
@@ -1,7 +1,20 @@
-var answer = prompt("are we there yet?");
 
-while (answer !== "yes") {
-    var answer = prompt("how about now?");
+var todos = [0];
+var input = prompt("What woud you like to do?");
+while (input !== "quit") {
+    if (input == "new") {
+        var item = prompt("add item");
+        todos.push(item);
+    }
+    else if ("list") {
+        todos.forEach(function(todo, i) {
+            console.log(i + ": " + todo);
+        });
+    }
+    else if ("delete") {
+        var item = prompt("item to delete?");
+        todos.splice(index, 1);
+    }
+    input = prompt("What would you like to do?");
 }
-
-alert("yeauuh bitch");
+console.log("Bye bye");
